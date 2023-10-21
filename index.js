1efe9e38ba6e83a1f49d1890a115414a9945608a
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-21 18:00:26 +0600

    project Done

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ const cors=require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = 3000
-
-// Add middleware
 app.use(cors());
 app.use(express.json());
 
 // mdrashadul898
 // YBOKnLT93ldMcj8j
 
-
-
-
 const uri = "mongodb+srv://mdrashadul898:YBOKnLT93ldMcj8j@cluster0.jds5c8p.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,9 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-
     const userCollection=client.db("userDbB").collection("newusers");
 
     // user create
@@ -54,6 +46,28 @@ async function run() {
         res.send(result);
     })
 
+    // Update user
+    app.put('/update/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const options = { upsert: true };
+        const updateproduct = req.body;
+
+        const product = {
+            $set: {
+                image: updateproduct.image,
+                name: updateproduct.name,
+                brand: updateproduct.brand,
+                types: updateproduct.types,
+                price: updateproduct.price,
+                reating: updateproduct.reating,
+                description: updateproduct.description
+            }
+        }
+
+        const result = await userCollection.updateOne(filter, product, options);
+        res.send(result);
+    })
 
 
 
@@ -61,8 +75,6 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
